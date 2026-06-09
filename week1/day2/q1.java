c08e48c226e0f3b2dea397dd5c5596381ddc9717
Ayush Singh <sayush.singhn@gmail.com>
2026-06-09 11:17:33 +0530

    Add Week1 Day2 Q1 Solution

---
+++ week1/day2/q1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int res = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (sum < 0) {
+                sum = 0;
+            }
+            sum = sum + nums[i];
+            if (sum > res) {
+                res = sum;
+            }
+        }
+        return res;
+    }
+}
